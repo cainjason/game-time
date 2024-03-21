f9b82d2c80f8dcae4e8999ffb504cbae813ba113
Jason Cain <118203097+cainjason@users.noreply.github.com>
2024-03-21 15:59:32 -0400

    chore(Setup) Bootstrapped tailwindcss

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="app">
+      <h1 class="text-3xl bg-green-300 text-red-500 font-bold underline">
+        Hello world!
+      </h1>
+    </div>
+  );
+}
+
+export default App;
